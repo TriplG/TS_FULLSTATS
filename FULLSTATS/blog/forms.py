a7ae29d3e7ac70d06647e18f1a26c3c05f6cd34c
Aleynik Aleksandr <sasha-aleyynik@mil.ru>
2022-05-25 13:43:00 +0300

    Added the forms.py file in which the application forms are located

---
+++ FULLSTATS/blog/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from .models import *
+
+
+class SortForm(forms.Form):
+    sort_form = forms.TypedChoiceField(label='Сортировать:', choices=[('дата', 'По дате'), ('рейтинг', 'По рейтингу')])
